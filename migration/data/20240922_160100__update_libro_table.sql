d669c310e13a744f9be4f7dd81b63e69f661104a
gabrio993 <gabriele93.fusco@gmail.com>
2024-09-22 17:23:36 +0200

    feat/add new file _...update_libro_table.sql_ into  migration/data/ to update the Database Libro Table

---
+++ migration/data/20240922_160100__update_libro_table.sql
@@ -0,0 +1,29 @@
+-- Aggiornamento per il libro con id 1
+UPDATE libro
+SET isbn = 9781234567890, anno_pubblicazione = 1972, genere = 'Politico'
+WHERE id_libro = 1;
+
+-- Aggiornamento per il libro con id 2
+UPDATE libro
+SET isbn = 9781234567891, anno_pubblicazione = 1980, genere = 'Romanzo storico'
+WHERE id_libro = 2;
+
+-- Aggiornamento per il libro con id 3
+UPDATE libro
+SET isbn = 9781234567892, anno_pubblicazione = 1957, genere = 'Romanzo'
+WHERE id_libro = 3;
+
+-- Aggiornamento per il libro con id 4
+UPDATE libro
+SET isbn = 9781234567893, anno_pubblicazione = 1929, genere = 'Romanzo esistenzialista'
+WHERE id_libro = 4;
+
+-- Aggiornamento per il libro con id 5
+UPDATE libro
+SET isbn = 9781234567894, anno_pubblicazione = 1949, genere = 'Romanzo autobiografico'
+WHERE id_libro = 5;
+
+-- Aggiornamento per il libro con id 6
+UPDATE libro
+SET isbn = 9781234567895, anno_pubblicazione = 1947, genere = 'Romanzo di formazione'
+WHERE id_libro = 6;
