841bd62a5b4169c829a03e0cefea4322489071dc
Francesco Labate <ICTS23-25.371@studenti.local>
2024-09-22 15:45:00 +0200

    Creazione file autore.php

---
+++ app/models/Autore.php
@@ -0,0 +1,18 @@
+
+
+<?php 
+/*
+Author: Francesco Labate
+Scope: Creation of the author class
+*/
+
+class Autore {
+    public int $id_autore; 
+    public string $nome; 
+    public string $cognome; 
+    public string $data_nascita; 
+}
+
+
+
+?>
