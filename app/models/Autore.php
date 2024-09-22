8977efaf1c117b1d203a4854adc0f49a740b233a
Francesco Labate <ICTS23-25.371@studenti.local>
2024-09-22 16:40:09 +0200

    Aggiunta metodi setter e miglioramento codice

--- app/models/Autore.php
+++ app/models/Autore.php
@@ -1,5 +1,3 @@
-
-
 <?php 
 /*
 Author: Francesco Labate
@@ -7,11 +5,20 @@ Scope: Creation of the author class
 */
 
 class Autore {
-
     public int $id_autore; 
     public string $nome; 
     public string $cognome; 
     public string $data_nascita; 
+
+    //Construct function 
+    public function __construct($id_autore,$nome,$cognome,$data_nascita)
+    {
+        $this->id_autore = $id_autore;
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->data_nascita = $data_nascita;
+    }
+
 
     //Methods of the class author (getters)
     public function getId_autore(){ 
@@ -29,9 +36,29 @@ class Autore {
     public function getData_nascita(){ 
         return $this->data_nascita; 
     }    
+
+
+    //methods setters 
+    public function setId_autore($id_autore)
+    {
+        $this->id_autore = $id_autore;
+    }
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function setCognome($cognome)
+    {
+        $this->cognome = $cognome
+    }
+
+    public function setData_nascita($data_nascita)
+    {
+        $this->data_nascita = $data_nascita;
+    }
+
 }
-
-
 
 
 
