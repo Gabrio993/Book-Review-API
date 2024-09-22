787cc2a59f4db8de0689f1c59c3af7f22f29fc38
Francesco Labate <ICTS23-25.371@studenti.local>
2024-09-22 16:24:22 +0200

    Aggiunti metodi getter alla classe autore

--- app/models/Autore.php
+++ app/models/Autore.php
@@ -7,11 +7,31 @@ Scope: Creation of the author class
 */
 
 class Autore {
+
     public int $id_autore; 
     public string $nome; 
     public string $cognome; 
     public string $data_nascita; 
+
+    //Methods of the class author (getters)
+    public function getId_autore(){ 
+        return $this->id_autore; 
+    }
+
+    public function getNome() {
+        return $this->nome; 
+    }
+    
+    public function getCognome() {
+        return $this->cognome; 
+    }
+
+    public function getData_nascita(){ 
+        return $this->data_nascita; 
+    }    
 }
+
+
 
 
 
