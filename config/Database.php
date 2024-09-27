d530afe44107b497512201ba7484502acb7de0b2
Alessandro Bo <alessandro.bo@edu.itspiemonte.it>
2024-09-27 11:49:49 +0200

    clonato db sul branch

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,19 @@
 <?php
+
+// Gestione della connessione al Database
+class Database
+{
+    private static $instance = null;
+
+    //Crea e restituisce un'unica connessione PDO al database
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            $config = include('config.php'); // Legge i dettagli della connessione dal file di configurazione
+            $dsn = "mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8";
+            self::$instance = new PDO($dsn, $config['db_user'], $config['db_password']);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    }
+}
