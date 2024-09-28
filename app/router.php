df1b5739a0f7b232b76b68efc847f25857188e8c
gabrio993 <gabriele93.fusco@gmail.com>
2024-09-28 12:19:02 +0200

    feat/ added route dispach control

--- app/router.php
+++ app/router.php
@@ -22,4 +22,36 @@ function route()
         $r->addRoute('DELETE', '/book-review-api/libri/{id:\d+}', ['LibroController', "deleteIdBook"]); // Cancella un libro (id)
 
     });
+
+    // Ottieni il metodo HTTP e l'URI della richiesta corrente
+    $httpMethod = $_SERVER['REQUEST_METHOD'];
+    $uri = $_SERVER['REQUEST_URI'];
+
+    // Rimuovi eventuali query string (es. ?page=1)
+    if (false !== $pos = strpos($uri, '?')) {
+        $uri = substr($uri, 0, $pos);
+    }
+    $uri = rawurldecode($uri);
+
+    // Esegui il dispatch della rotta
+    $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+    switch ($routeInfo[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            // Errore 404
+            echo "404 - Not Found";
+            break;
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            // Metodo HTTP non consentito
+            echo "405 - Method Not Allowed";
+            break;
+        case FastRoute\Dispatcher::FOUND:
+            // Se la rotta è trovata, chiama il controller e il metodo associati
+            $handler = $routeInfo[1];  // Esempio: ['LibriController', 'index']
+            $vars = $routeInfo[2];  // Variabili passate nella rotta (es. ID o altri parametri)
+
+            // Istanzia il controller e chiama il metodo specifico
+            $controller = new $handler[0];
+            call_user_func_array([$controller, $handler[1]], $vars);
+            break;
+    }
 }
