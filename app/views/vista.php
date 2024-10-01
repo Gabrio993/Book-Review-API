5528b17a91e650ff7bf5228b3689cc69cbee7fce
gabrio993 <gabriele93.fusco@gmail.com>
2024-10-01 18:24:38 +0200

    feat/added a modal for a better UX in the crud operation

--- app/views/vista.php
+++ app/views/vista.php
@@ -116,3 +116,22 @@
             </div>
         </div>
     </form>
+
+    <!-- Modale di Conferma per le varie operazioni dell'utente -->
+    <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmModalLabel">Conferma Operazione</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Sei sicuro di voler procedere con questa operazione?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <button type="button" class="btn btn-danger" id="confirmButton">Conferma</button>
+                </div>
+            </div>
+        </div>
+    </div>
