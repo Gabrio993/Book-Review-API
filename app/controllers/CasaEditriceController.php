f820b7c61891af191b3c96c9da1c3fac91f8fd56
Alessandro Bo <alessandro.bo@edu.itspiemonte.it>
2024-10-04 12:58:16 +0200

    aggiunte funzioni da sviluppare in CasaEditriceController

--- app/controllers/CasaEditriceController.php
+++ app/controllers/CasaEditriceController.php
@@ -1,2 +1,19 @@
 <?php
 
+class CasaEditriceController
+{
+  public function getAllCaseEditrici()
+  {
+    // da fare
+  }
+
+  public function getCasaEditriceById($id_casa_editrice)
+  {
+    // da fare
+  }
+
+  public function createCasaEditrice($nome, $anno_fondazione, $paese)
+  {
+    // da fare
+  }
+}
--- public/index.php
+++ public/index.php
@@ -1,31 +1,30 @@
 <?php
 
-require_once "../vendor/autoload.php"; // Inclusione dell'autoloader di Composer
-require_once "../app/models/CasaEditrice.php"; // Inclusione del model 
+require_once "../vendor/autoload.php"; // Composer autoloader
+require_once "../app/models/CasaEditrice.php"; 
 require_once "../app/models/CasaEditriceDAO.php";
-require_once "../app/controllers/CasaEditriceController.php"; // Inclusione del controller 
-require_once "../config/Database.php"; // Inclusione della connessione al db
+require_once "../app/controllers/CasaEditriceController.php"; // Controller 
+require_once "../config/Database.php"; // DB
 
+/******* Testing *******/
 
-// Testing
-
-/*------------Ottieni tutti gli utenti------------*/
+/******* Ottieni tutte le case editrici *******/
 echo "Ottenimento di tutte le case editrici";
 $controller = new CasaEditriceController();
 echo "<pre>";
 $controller->getAllCaseEditrici();
 echo "</pre>";
 
-/*------------Trova Autore per id------------*/
+/******** Trova casa editrice per id ********/
 echo "Ottenimento casa editrice in base all'ID inserito";
 $controller2 = new CasaEditriceController();
 echo "<pre>";
 $controller2->getCasaEditriceById(5);
 echo "</pre>";
 
-/*------------Inserimento nuovo autore------------*/
+/******** Inserimento nuova casa editrice ********/
 echo "Aggiungo una nuova casa editrice ";
 $controller3 = new CasaEditriceController();
 echo "<pre>";
 $controller3->createCasaEditrice('Salani',"1998","London");
-echo "</pre>;
+echo "</pre>";
