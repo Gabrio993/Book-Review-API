dff129d6a5cb6975f151bd351467dada83d9deb0
Alessandro Bo <alessandro.bo@edu.itspiemonte.it>
2024-10-04 12:38:02 +0200

    cancellazione di file errati e correzioni minori

--- app/models/CasaEditrice.php
+++ app/models/CasaEditrice.php
@@ -1,7 +1,6 @@
 <?php
 
 class CasaEditrice {
-
   // definizione delle proprietà
   public $id_casa_editrice;
   public $nome;
@@ -9,9 +8,7 @@ class CasaEditrice {
   public $paese;
 
   // metodo costruttore
-  public function __construct($id_casa_editrice = null, $nome = null, $anno_fondazione = null, $paese = null)
-  {
-
+  public function __construct($id_casa_editrice, $nome = null, $anno_fondazione = null, $paese = null) {
     $this->id_casa_editrice = $id_casa_editrice;
     $this->nome = $nome;
     $this->anno_fondazione = $anno_fondazione;
