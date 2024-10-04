009475f671821f6788ba19990677f1cd2129037b
Serena Finis <serena.finis@edu.itspiemonte.it>
2024-10-04 12:10:18 +0200

    feat: add testing code and reorder requires

--- public/index.php
+++ public/index.php
@@ -1,25 +1,47 @@
 <?php
 
-require_once "../vendor/autoload.php"; // Inclusione dell'autoloader di Composer
-require_once "../app/models/Utente.php"; // Inclusione del model 
-require_once "../app/controllers/UtenteController.php"; // Inclusione del controller 
-require_once "../config/Database.php"; // Inclusione della connessione al db
+
+require_once "../vendor/autoload.php"; // inclusione dell'autoloader di Composer
+require_once "../config/Database.php"; // inclusione della connessione al db
+require_once "../app/models/Utente.php"; // inclusione del model
+require_once "../app/models/UtenteDAO.php"; // inclusione del model 
+require_once "../app/controllers/UtenteController.php"; // inclusione del controller 
 
 
 // Testing
 
 /*------------Ottieni tutti gli utenti------------*/
-echo "Ottieni tutti gli utenti";
-$controller = new UtenteController();
-echo "<pre>";
-$controller->getAllUsers();
-echo "</pre>";
+// echo "Ottieni tutti gli utenti";
+// $controller = new UtenteController();
+// echo "<pre>";
+// $controller->getAllUsers();
+// echo "</pre>";
+/*------------Trova utente per id------------*/
+// echo "Trova utente per id";
+// $controller2 = new UtenteController();
+// echo "<pre>";
+// $controller2->getUserById(2);
+// echo "</pre>";
+
 /*------------Trova utente per id------------*/
 echo "Trova utente per id";
 $controller2 = new UtenteController();
 echo "<pre>";
-$controller2->getUserById(2);
+$controller2->showUserId(2);
 echo "</pre>";
+
+
 /*------------Crea un nuovo Utente------------*/
-// $controller2->createUser(6,"pippoPower","spippo@gmail.com","pippo98");
+// $controller2->create("giorgio","giorgio@gmail.com","giorgio!");
+/*------------Crea un nuovo Utente------------*/
+$data = [
+  'username' => 'giorgio',
+  'email' => 'giorgio@gmail.com',
+  'password' => 'giorgio!'
+];
+
+// Simula la lettura di input JSON
+file_put_contents('php://input', json_encode($data));
+
+$controller2->create();
 
