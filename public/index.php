9369090772e7c7b6d0b67f4976a97a41000f7c22
Gabriele Chiabrero <gabriele.chiabrero@edu.itspiemonte.it>
2024-10-04 12:08:36 +0200

    modifica file per testare le funzionalità

--- public/index.php
+++ public/index.php
@@ -10,22 +10,22 @@ require_once "../config/Database.php"; // Inclusione della connessione al db
 // Testing
 
 /*------------Ottieni tutti gli utenti------------*/
-echo "Ottenimento di tutti gli autori";
+echo "Ottenimento di tutte le case editrici";
 $controller = new CasaEditriceController();
 echo "<pre>";
 $controller->getAllCaseEditrici();
 echo "</pre>";
 
 /*------------Trova Autore per id------------*/
-echo "Ottenimento autore in base all'ID inserito";
+echo "Ottenimento casa editrice in base all'ID inserito";
 $controller2 = new CasaEditriceController();
 echo "<pre>";
 $controller2->getCasaEditriceById(5);
 echo "</pre>";
 
 /*------------Inserimento nuovo autore------------*/
-echo "Aggiungo un nuovo autore ";
+echo "Aggiungo una nuova casa editrice ";
 $controller3 = new CasaEditriceController();
 echo "<pre>";
 $controller3->createCasaEditrice('Salani',"1998","London");
-echo "</pre>";
+echo "</pre>;
