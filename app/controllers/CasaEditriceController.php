4aa2abbbc67912bc0e983a32045edcaaff679d81
Gabriele Chiabrero <gabriele.chiabrero@edu.itspiemonte.it>
2024-10-05 08:59:44 +0200

    aggiunta dei metodi del file CasaEditriceController

--- app/controllers/CasaEditriceController.php
+++ app/controllers/CasaEditriceController.php
@@ -4,16 +4,30 @@ class CasaEditriceController
 {
   public function getAllCaseEditrici()
   {
-    // da fare
+    $casaEditrice = new CasaEditriceDAO();
+    $caseEditrici = $casaEditrice->getAllCaseEditrici();
+    echo json_encode($caseEditrici, JSON_PRETTY_PRINT);
   }
 
   public function getCasaEditriceById($id_casa_editrice)
   {
-    // da fare
+    $casaEditrice = new CasaEditriceDAO();
+    $casaEditriceId = $casaEditrice->getCasaEditriceById($id_casa_editrice);
+
+    if($casaEditriceId)
+      echo json_encode($casaEditriceId, JSON_PRETTY_PRINT);
+    else
+      echo json_encode(["message" => "Casa editrice non trovata"], JSON_PRETTY_PRINT);
   }
 
   public function createCasaEditrice($nome, $anno_fondazione, $paese)
   {
-    // da fare
+    $casaEditrice = new CasaEditriceDAO();
+    $casaEditrice->createCasaEditrice($nome, $anno_fondazione, $paese);
+
+    if($casaEditrice)
+      echo json_encode(["message" => "Casa editrice inserita correttamente"], JSON_PRETTY_PRINT);
+    else
+      echo json_encode(["message" => "Casa editrice non è stata inserita correttamente"], JSON_PRETTY_PRINT);
   }
 }
