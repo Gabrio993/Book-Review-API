b86835397cf70e3f2e44d594873ada7f6a2e54ee
gabrio993 <gabriele93.fusco@gmail.com>
2024-10-06 12:35:03 +0200

    feat/modified CasaEditriceController.phm on Create method

--- app/controllers/CasaEditriceController.php
+++ app/controllers/CasaEditriceController.php
@@ -14,18 +14,26 @@ class CasaEditriceController
     $casaEditrice = new CasaEditriceDAO();
     $casaEditriceId = $casaEditrice->getCasaEditriceById($id_casa_editrice);
 
-    if($casaEditriceId)
+    if ($casaEditriceId)
       echo json_encode($casaEditriceId, JSON_PRETTY_PRINT);
     else
       echo json_encode(["message" => "Casa editrice non trovata"], JSON_PRETTY_PRINT);
   }
 
-  public function createCasaEditrice($nome, $anno_fondazione, $paese)
+  public function createCasaEditrice()
   {
+    // Leggi il corpo della richiesta
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Accedi ai dati decodificati
+    $nome = $data["nome"] ?? null;
+    $anno_fondazione = $data["anno_fondazione"] ?? null;
+    $paese = $data["paese"] ?? null;
+
     $casaEditrice = new CasaEditriceDAO();
     $casaEditrice->createCasaEditrice($nome, $anno_fondazione, $paese);
 
-    if($casaEditrice)
+    if ($casaEditrice)
       echo json_encode(["message" => "Casa editrice inserita correttamente"], JSON_PRETTY_PRINT);
     else
       echo json_encode(["message" => "Casa editrice non è stata inserita correttamente"], JSON_PRETTY_PRINT);
