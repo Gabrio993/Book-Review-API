242c18fd12d4a5a22824b37cc8c5c2d2423ba33b
gabrio993 <gabriele93.fusco@gmail.com>
2024-10-06 13:39:34 +0200

    modified class AutoreController in methods: create, update and delete

--- app/controllers/Autore_controller.php
+++ app/controllers/Autore_controller.php
@@ -26,30 +26,50 @@ class AutoreController
         }
     }
 
-    public function createAuthors($id_autore, $nome, $cognome, $data_nascita){
+    public function createAuthors()
+    {
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        $id_autore = $data["id_autore"] ?? null;
+        $nome = $data["nome"] ?? null;
+        $cognome = $data["cognome"] ?? null;
+        $data_nascita = $data["data_nascita"] ?? null;
         //conversione della data nel formato corretto
-        $data = strtotime($data_nascita);
-        $new_data_nascita = date("Y-m-d",$data);
-       // crea un'istanza della classe Autore
-       $autore = new AutoreDAO();
-       //Creo il nuovo autore
-       $autore->createAuthors($id_autore, $nome, $cognome, $new_data_nascita);
-
-
+        $new_data_nascita = strtotime($data_nascita);
+        $final_data = date("Y-m-d", $new_data_nascita);
+        // crea un'istanza della classe Autore
+        $autore = new AutoreDAO();
+        //Creo il nuovo autore
+        $autore->createAuthors($id_autore, $nome, $cognome, $final_data);
     }
 
-    public function deleteAuthors($id_autore) {
+    public function deleteAuthors($id_autore)
+    {
         // crea un'istanza della classe Autore
-        $autore = new AutoreDAO(); 
+        $autore = new AutoreDAO();
         // Elimino l'autore dato l'Id
         $autore->deleteAuthors($id_autore);
 
+        // Output
+        if ($autore) {
+            json_encode(["message" => "Autore cancellato con successo"], JSON_PRETTY_PRINT);
+        } else {
+            json_encode(["message" => "Errore nella cancellazione dell'autore"], JSON_PRETTY_PRINT);
+        }
     }
 
-    public function updateAuthors($id_autore, $nome, $cognome, $data_nascita) {
-        $autore = new AutoreDAO(); 
+    public function updateAuthors($id_autore)
+    {
+        // Otteniamo i dati JSON inviati dal client
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $id_autore = $data["id_autore"] ?? null;
+        $nome = $data["nome"] ?? null;
+        $cognome = $data["cognome"] ?? null;
+        $data_nascita = $data["data_nascita"] ?? null;
+
+        $autore = new AutoreDAO();
         $autore->updateAuthors($id_autore, $nome, $cognome, $data_nascita);
     }
-      
-
 }
