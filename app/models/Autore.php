253f85ba2e9dd91922643804442ffe0abefa8fc6
gabrio993 <gabriele93.fusco@gmail.com>
2024-10-11 10:34:58 +0200

    not important modifies

--- app/models/Autore.php
+++ app/models/Autore.php
@@ -1,28 +1,20 @@
-<?php 
-/*
-Author: Francesco Labate
-Scope: Creation of the author class
-*/
+<?php
 
-class Autore {
+class Autore
+{
 
-    public $id_autore; 
-    public $nome; 
-    public $cognome; 
-    public $data_nascita; 
+    public $id_autore;
+    public $nome;
+    public $cognome;
+    public $data_nascita;
 
     //Construct function 
-    public function __construct($id_autore=null,$nome=null,$cognome=null,$data_nascita=null)
+    public function __construct($id_autore = null, $nome = null, $cognome = null, $data_nascita = null)
     {
-       
+
         $this->id_autore = $id_autore;
         $this->nome = $nome;
         $this->cognome = $cognome;
         $this->data_nascita = $data_nascita;
     }
-
 }
-
-
-
-?>
